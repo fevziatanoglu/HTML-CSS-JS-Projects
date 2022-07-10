f9e74d08c764719b997167f75b0028a4afb9054e
Fevzi Atanoğlu <fevziatan2@gmail.com>
2022-07-10 11:46:22 +0300

    HTML CSS JS Save Person App

--- HTML CSS JS Save Person App/script.js
+++ HTML CSS JS Save Person App/script.js
@@ -17,6 +17,10 @@ const form = document.querySelector("#form");
 const errorInfo = document.querySelector(".info-error");
 const successInfo = document.querySelector(".info-success");
 
+const submitButton =  document.querySelector("#submit-btn");
+
+let satir;
+
 
 
 form.addEventListener('submit',savePerson);
@@ -29,6 +33,9 @@ function savePerson(e){
     if(checkData(person)){
 
         savePersonToList(person);
+        
+
+
         errorInfo.style.display = "none";
         successInfo.style.display = "block";
 
@@ -70,7 +77,7 @@ function savePersonToList(person){
 
     const addPerson = document.createElement("tr");
     addPerson.innerHTML = `
-                <tr>
+                <tr class=" u-full-width">
                 <td>${nameInput.value}</td>
                 <td>${surnameInput.value}</td>
                 <td>${emailInput.value}</td>
@@ -80,16 +87,37 @@ function savePersonToList(person){
                 </tr>`
 
                 personList.appendChild(addPerson);
+                satir.remove();
+                satir = null;
+                submitButton.value = "Save";
+                submitButton.style.backgroundColor = "#33C3F0";
 }
+
+
 
 
 personList.addEventListener("click" , personEvents);
 
 function personEvents(e){
-    
+
     if(e.target.classList.contains("delete-btn")){
-    e.target.parentElement.parentElement.remove();
+    deletePerson(e.target);
+    }
+
+    if(e.target.classList.contains("edit-btn")){
+        satir = e.target.parentElement.parentElement;
+        submitButton.value = "Edit";
+        //old color  #33C3F0
+        submitButton.style.backgroundColor = "green";
+
+        nameInput.value = satir.children[0].textContent;
+        surnameInput.value = satir.children[1].textContent;
+        emailInput.value = satir.children[2].textContent;
     }
     
     
 }
+
+function deletePerson(target){
+    target.parentElement.parentElement.remove();
+    }
