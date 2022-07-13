7523daa73005de867b88bc718a56ec2a4fe57bd9
Fevzi Atanoğlu <fevziatan2@gmail.com>
2022-07-13 13:56:50 +0300

    HTML CSS JS QuizApp

--- HTML CSS JS QuizApp/script.js
+++ HTML CSS JS QuizApp/script.js
@@ -1,12 +1,3 @@
-const textA = document.querySelector("#a-text");
-const textB = document.querySelector("#b-text");
-const textC = document.querySelector("#c-text");
-const textD = document.querySelector("#d-text");
-const answers = document.querySelectorAll(".answer-radio");
-const textQuestion = document.querySelector("#question-text");
-
-let corretAnswerAmount =0;
-
 const quizData = [
     {
     question : "My name is ?",
@@ -27,14 +18,43 @@ const quizData = [
         }
 ]
 
+const textA = document.querySelector("#a-text");
+const textB = document.querySelector("#b-text");
+const textC = document.querySelector("#c-text");
+const textD = document.querySelector("#d-text");
+const answers = document.querySelectorAll(".answer-radio");
+const textQuestion = document.querySelector("#question-text");
 const nextButton = document.querySelector("#next-btn");
-
+let corretAnswerAmount =0;
 let currentQuestionIndex =0;
 const currentQuestion = quizData[currentQuestionIndex];
 
 
+loadQuestion();
+nextButton.addEventListener('click' , nextQuestion);
 
 
+function nextQuestion(){
+    let isCorrect = false;
+    answers.forEach(answer => {
+        if(answer.checked){
+            if(answer.value == currentQuestion.correct){isCorrect = true;}      
+            }
+        }
+    );
+
+    if(isCorrect){corretAnswerAmount++;}
+    
+    
+    if(currentQuestionIndex+1 >= quizData.length){
+        alert(`Quiz is over , you have ${corretAnswerAmount} correct answers!`);}   
+    else{
+        currentQuestionIndex++;
+        loadQuestion();  
+    }
+      
+    
+}
 
 function loadQuestion(){
     const currentQuestion = quizData[currentQuestionIndex];
@@ -44,40 +64,3 @@ function loadQuestion(){
     textC.innerHTML = currentQuestion.c;
     textD.innerHTML = currentQuestion.d;
 }
-
-loadQuestion();
-
-function nextQuestion(){
-
-    let isCorrect = false;
-
-    answers.forEach(answer => {
-        if(answer.checked){
-            if(answer.value = currentQuestion.correct){
-                isCorrect = true;
-            }
-        }
-    });
-
-    if(isCorrect){
-        corretAnswerAmount++;
-    }
-
-    if(currentQuestionIndex+1 >= quizData.length){
-        alert(`Quiz is over , you have ${corretAnswerAmount} correct answers!`);
-    }else{
-        currentQuestionIndex++;
-        loadQuestion();  
-    }
-      
-    
-}
-
-nextButton.addEventListener('click' , nextQuestion);
-
-answers.forEach(answer => {
-    if(answer.checked){
-        console.log(answer.value);
-    }
-});
-
