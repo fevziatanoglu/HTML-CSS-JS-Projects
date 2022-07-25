fb37bc8b5803b7f4de148c5d94aa3f30ac40f9e7
Fevzi Atanoğlu <fevziatan2@gmail.com>
2022-07-25 12:53:18 +0300

    HTML CSS JS Calculator

--- HTML CSS JS Calculator/script.js
+++ HTML CSS JS Calculator/script.js
@@ -1,11 +1,36 @@
-const buttons = document.querySelectorAll(".number-btn");
+const buttons = document.querySelectorAll(".btn");
 const buttonsArray = Array.from(buttons);
-
 const screen = document.querySelector("#screen");
+
+var writeOperator = false;
+
+
 buttonsArray.forEach(button => {
+
     button.addEventListener("click" , function(e){
-        screen.value += button.innerText;
-        console.log(button.innerText);
+
+        if(button.classList.contains("number-btn")){
+             
+             screen.value += button.innerText;
+             console.log(button.innerText);
+
+             
+        }
+
+        else if(button.classList.contains("operation-btn") && writeOperator){
+            screen.value += button.innerText;
+             console.log(button.innerText);
+
+             writeOperator = false;
+             writeNumber = true;
+        }
+        
+        else if(button.classList.contains("equal-btn") && writeOperator){
+            screen.value = eval(screen.value);
+        }
+
+       
+
     });
 
 })
