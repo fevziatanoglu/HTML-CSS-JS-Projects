ac5b3b9e4250d3283b3de6cec510ca8db9115805
Fevzi Atanoğlu <fevziatan2@gmail.com>
2022-07-27 15:12:09 +0300

    HTML CSS JS Memory Card Game

--- HTML CSS JS Memory Game/script.js
+++ HTML CSS JS Memory Game/script.js
@@ -3,37 +3,86 @@ const cardsList = Array.from(cards);
 
 
 var clickChange =   2;
+var firstCard;
+var secondCard;
+
+function closeCards(){
+    firstCard.children[1].style.display = "none";
+    firstCard.children[0].style.display = "block";
+
+    secondCard.children[1].style.display = "none";
+    secondCard.children[0].style.display = "block";
+}
+
+function openCard(card){
+    card.children[1].style.display = "block";
+    card.children[0].style.display = "none";
+    clickChange--;
+}
+
+function animateCard(card,animation){
+    card.style =`animation: ${animation} 0.35s ease-in-out`;
+}
+
+function clickCard(card){
+    animateCard(card,"flip");
+
+    if(clickChange > 0){
+
+        if(clickChange ==2) {
+            firstCard = card;
+        }
+        else if(clickChange == 1){
+            secondCard = card;
+        }
+        
+        openCard(card);   
+    }
+
+}
+
+function controlCards(card){
+    if(clickChange == 0)
+    {
+     setTimeout(function(){
+      if(!(firstCard.children[1].src == secondCard.children[1].src)){ 
+             animateCard(firstCard,"shake");
+             animateCard(secondCard,"shake");}
+            
+            else{
+                animateCard(firstCard,"up");
+                animateCard(secondCard,"up");
+
+                firstCard.style.backgroundColor="green";
+                secondCard.style.backgroundColor="green";
+        }
+        } , 500);
 
 
+     setTimeout(function(){
+      if(!(firstCard.children[1].src == secondCard.children[1].src)){  
+        closeCards();
+      }
 
+      clickChange=2;} , 1000);
+
+    }
+ 
+}
 
 cardsList.forEach(card =>{
 card.addEventListener("click" , function(e){
-    console.log(clickChange);
-
-    if(clickChange > 0){
-        card.children[1].style.display = "block";
-        card.children[0].style.display = "none";
-        clickChange--;
         
+        clickCard(card);
+        controlCards(card);
+                }
+            );
+        }
+    );
+         
 
-           
-            
     
-    }
-        
-        setTimeout(function(){
-        console.log("1.5 saniye oldu");
-        card.children[1].style.display = "none";
-        card.children[0].style.display = "block";
-        clickChange++;
-        } , 1500);
-    
-                
- 
-  
-            }  
-         );
 
-    }
-);
+
+
+
